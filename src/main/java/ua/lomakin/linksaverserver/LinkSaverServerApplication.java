6f889e2f6a450f54e58abf952f71da160158fe29
Lomakin Dmytro <menu682@gmail.com>
2021-12-16 20:32:59 +0200

    created

---
+++ src/main/java/ua/lomakin/linksaverserver/LinkSaverServerApplication.java
@@ -0,0 +1,13 @@
+package ua.lomakin.linksaverserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkSaverServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkSaverServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/lomakin/linksaverserver/LinkSaverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lomakin.linksaverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkSaverServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
