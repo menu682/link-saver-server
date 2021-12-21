0ff40105051cd7213325e37be98eb541bb1fe8cb
Lomakin Dmytro <menu682@gmail.com>
2021-12-21 22:12:07 +0200

    v.02 beta done

--- src/main/java/ua/lomakin/linksaverserver/DTO/categoryDTO/CategoryDTO.java
+++ src/main/java/ua/lomakin/linksaverserver/DTO/categoryDTO/CategoryDTO.java
@@ -1,9 +1,6 @@
 package ua.lomakin.linksaverserver.DTO.categoryDTO;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 public class CategoryDTO {
